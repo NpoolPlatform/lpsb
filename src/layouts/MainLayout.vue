7678fe62ad050f007ae49ae55b40c448d9f3cecc
lidaqiang <dqli161@163.com>
2022-09-13 14:49:33 +0800

    Reset Header Style

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,15 +1,17 @@
 <template>
-  <MainHeader v-if='setting.ShowMainHeader' :class='[ special ? "special" : "" ]' />
-  <div :class='[ "main row", special ? "special" : "" ]'>
-    <SideMenu />
-    <div id='main'>
-      <div :class='[ setting.ShowSideMenu ? "with-sidebar" : "" ]'>
-        <router-view />
+  <div class='page column'>
+    <MainHeader v-if='setting.ShowMainHeader' :class='[ special ? "special" : "" ]' />
+    <div :class='[ "main row", special ? "special" : "" ]'>
+      <SideMenu />
+      <div id='main'>
+        <div :class='[ setting.ShowSideMenu ? "with-sidebar" : "" ]'>
+          <router-view />
+        </div>
       </div>
     </div>
+    <q-space />
+    <Footer v-if='setting.ShowFooterTop' :class='[ special ? "special" : "" ]' />
   </div>
-  <q-space />
-  <Footer v-if='setting.ShowFooterTop' :class='[ special ? "special" : "" ]' />
   <LangLoader />
   <Prepare />
 </template>
