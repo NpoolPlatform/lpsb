a8f8be9bd87b42c73d0903fe7870eb4cbedb9f3f
lidaqiang <dqli161@163.com>
2022-09-14 11:42:14 +0800

    Change App ID

--- src/const/const.ts
+++ src/const/const.ts
@@ -1,6 +1,6 @@
-const LogoText = 'PROCYON'
+const LogoText = 'LP SECRET BASE'
 
-const AppID = 'ff2c5d50-be56-413e-aba5-9c7ad888a769'
+const AppID = 'ef936d96-d8bb-40ee-a9b6-8c5af3d41bf8'
 const ThrottleSeconds = 10
 const DefaultGoodID = 'de420061-e878-4a8b-986a-805cadd59233'
 
