0633ac72fc4a6d32d3f166951fefeeef6f6c44b1
lidaqiang <dqli161@163.com>
2022-09-26 14:26:32 +0800

    Legal Bugfix

--- src/pages/Legal.vue
+++ src/pages/Legal.vue
@@ -1,12 +1,24 @@
 <template>
   <div class='content legal'>
-    <Legal />
+    <LegalPage />
   </div>
 </template>
 
 <script setup lang='ts'>
-import { defineAsyncComponent } from 'vue'
+import { defineAsyncComponent, onMounted, watch } from 'vue'
+import { useI18n } from 'vue-i18n'
+const { locale } = useI18n({ useScope: 'global' })
 
-const Legal = defineAsyncComponent(() => import('src/components/help/Legal.vue'))
+const LegalPage = defineAsyncComponent(() => import('src/pages/legal/ja-JP/Legal.vue'))
+
+watch(locale, () => {
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/restrict-template-expressions
+  LegalPage.value = defineAsyncComponent(() => import(`src/pages/legal/${locale.value}/Legal.vue`))
+})
+
+onMounted(() => {
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/restrict-template-expressions
+  LegalPage.value = defineAsyncComponent(() => import(`src/pages/legal/${locale.value}/Legal.vue`))
+})
 
 </script>
