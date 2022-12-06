35aa2bc73edf01a32b52b7a40ec77bf356b0b323
lidaqiang <dqli161@163.com>
2022-12-06 15:22:46 +0800

    refactor affiliates

--- src/localstore/ledger/types.ts
+++ src/localstore/ledger/types.ts
@@ -1,3 +1,5 @@
+import { GoodArchivement, GoodProfit, UserArchivement } from 'npool-cli-v4'
+
 interface BalanceGeneral{
   CoinTypeID: string;
   CoinName: string;
@@ -29,6 +31,23 @@ interface CoinProfit {
   Last30DayAmount: number;
   Last30DayUSDAmount: number;
   Last30DayJPYAmount: number;
+}
+
+export interface MyGoodProfit extends GoodProfit {
+  CoinPreSale: boolean;
+  TotalInComing: number;
+  TotalUSDInComing: number;
+  Last24HoursInComing: number;
+  Last24HoursUSDInComing: number;
+  Last30DaysInComing: number;
+  Last30DaysUSDInComing: number;
+}
+
+export interface MyGoodArchivement extends GoodArchivement {
+  Editing: boolean;
+}
+export interface MyArchivement extends UserArchivement {
+  Archivements: MyGoodArchivement[];
 }
 
 export {
