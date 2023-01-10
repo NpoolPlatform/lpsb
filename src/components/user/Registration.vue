a6bdfed6431283b0d12013bc6fa90647bac178f3
lidaqiang <dqli161@163.com>
2023-01-10 14:58:50 +0800

    bugfix: registration

--- src/components/user/Registration.vue
+++ src/components/user/Registration.vue
@@ -164,7 +164,7 @@ const onConfirmInvCodeErrorFocusOut = () => {
     invCodeError.value = true
     return
   }
-  invCodeError.value = invitationCode.value?.length !== 14
+  invCodeError.value = invitationCode.value?.length !== 12
 }
 
 const invitationCode = ref(originInvitationCode.value)
