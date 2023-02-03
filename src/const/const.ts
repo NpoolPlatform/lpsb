b15c47954eaef7e336ba2094ca2e61a7d9a38c97
lidaqiang <dqli161@163.com>
2023-02-03 17:12:10 +0800

    change default GoodID

--- src/const/const.ts
+++ src/const/const.ts
@@ -2,7 +2,7 @@ const LogoText = 'LP SECRET BASE'
 
 const AppID = '9141b71a-ff6e-4229-8cf5-cc71459e9803'
 const ThrottleSeconds = 10
-const DefaultGoodID = 'de420061-e878-4a8b-986a-805cadd59233'
+const DefaultGoodID = 'aacaa886-9801-4673-97c0-39c4ac64cdd3'
 
 enum IntervalKey {
   All = 'All',
