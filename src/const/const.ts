c751ec665b7caa07e2ee328b1a4d6c20e0cdd0b5
lidaqiang <dqli161@163.com>
2023-03-13 12:03:25 +0800

    delete DefaultGoodID

--- src/const/const.ts
+++ src/const/const.ts
@@ -2,7 +2,6 @@ const LogoText = 'LP SECRET BASE'
 
 const AppID = '9141b71a-ff6e-4229-8cf5-cc71459e9803'
 const ThrottleSeconds = 10
-const DefaultGoodID = 'aacaa886-9801-4673-97c0-39c4ac64cdd3'
 
 enum IntervalKey {
   All = 'All',
@@ -14,6 +13,5 @@ export {
   LogoText,
   AppID,
   ThrottleSeconds,
-  DefaultGoodID,
   IntervalKey
 }
