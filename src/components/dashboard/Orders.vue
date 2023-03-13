6078dc0f1269ad7c1da8563c5d36b0ad52b4c282
lidaqiang <dqli161@163.com>
2023-03-13 11:28:27 +0800

    support export benefit & orders

--- src/components/dashboard/Orders.vue
+++ src/components/dashboard/Orders.vue
@@ -34,6 +34,7 @@ const orders = computed(() => order.orders)
 const detail = useFrontendDetailStore()
 const good = useAdminAppGoodStore()
 const getDeservedRatio = computed(() => (goodID: string) => 1 - Number(good.getGoodByID(goodID)?.TechnicalFeeRatio) / 100)
+
 interface ExportOrder {
   CreatedAt: string;
   ProductType: string;
@@ -48,6 +49,7 @@ interface ExportOrder {
   ProfitCurrency: string;
   OrderStatus: OrderState;
 }
+
 const exportOrders = computed(() => Array.from(orders.value).map((el) => {
   return {
     CreatedAt: new Date(el.CreatedAt * 1000).toISOString()?.replace('T', ' ')?.replace('.000Z', ' UTC'),
@@ -64,6 +66,7 @@ const exportOrders = computed(() => Array.from(orders.value).map((el) => {
     OrderStatus: el.State
   } as ExportOrder
 }))
+
 const onExportClick = () => {
   const output = stringify(exportOrders.value, {
     header: true,
